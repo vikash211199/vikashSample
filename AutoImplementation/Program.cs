aadae5ad981d26e5dec33ac2194ce1b2ae60cc9a
vikash2199 <vikashrajavel2199@gmail.com>
2022-03-02 19:49:29 +0530

    first commit

---
+++ AutoImplementation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoImplementation
+{
+    internal class Program
+    {
+        class Product
+        {
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public int Price { get; set; }
+        }
+        static void Main()
+        {
+            Product product = new Product()
+            {
+                ProductId = 1,
+                ProductName = "Keyboard",
+                Price = 450
+            };
+            Console.WriteLine(product.ProductId);
+            Console.WriteLine(product.ProductName);
+            Console.WriteLine(product.Price);
+            Console.ReadKey();
+        }
+    }
+}
